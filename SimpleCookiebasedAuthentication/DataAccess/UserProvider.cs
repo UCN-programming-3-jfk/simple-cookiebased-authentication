52152dcca0960364a991f341c11680a964553ceb
Jakob Farian Krarup <kr@rup.dk>
2022-11-10 21:44:13 +0100

    case insensitive username checking

--- SimpleCookiebasedAuthentication/DataAccess/UserProvider.cs
+++ SimpleCookiebasedAuthentication/DataAccess/UserProvider.cs
@@ -8,11 +8,11 @@ public class UserProvider : IUserProvider
         //This code simulates accessing a database
         //and retrieving the user based on the username and password
 
-        if (userName == "Bob" && password == "1234")
+        if (userName.ToLower().Equals("bob") && password == "1234")
         {
             return new User() { Id = 42, UserName = "Bob", Email = "bob@gmail.com", Password = "1234", Role = UserRole.User };
         }
-        else if (userName == "Admin" && password == "1234")
+        else if (userName.ToLower().Equals("admin") && password == "1234")
         {
             return new User() { Id = 1337, UserName = "Administrator", Email = "admin@gmail.com", Password = "1234", Role = UserRole.Administrator };
         }
